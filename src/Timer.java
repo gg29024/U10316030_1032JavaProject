78abd3049d220769eac0a0ab4e778c02911423c2
gg29024 <gg29024@gmail.com>
2015-06-12 19:52:15 +0800

    2

--- src/Timer.java
+++ src/Timer.java
@@ -1,8 +1,14 @@
 
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.util.*;
 import java.awt.event.*;
+import java.util.Date;
+
+import java.util.TimerTask;
+
 
 
 public class Timer extends JFrame{
@@ -15,7 +21,9 @@ public class Timer extends JFrame{
 		add(object,BorderLayout.NORTH);
 		add(object2,BorderLayout.CENTER);
 		add(object3,BorderLayout.SOUTH);
-		
+		EggTimer eggTimer = new EggTimer(1);
+	    eggTimer.start();
+
 	}
 		
 	public static void main(String[] args){
@@ -26,7 +34,8 @@ public class Timer extends JFrame{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
-	}
+		
+			}
 }
 
 
@@ -86,9 +95,16 @@ class Numbers extends JPanel{
 
 class Push extends JPanel{
 	
+	int hour;
+	int min;
+	int sec;
+	
+	private JTextField jtfHour = new JTextField(5);
+	private JTextField jtfMinute = new JTextField(5);
+	private JTextField jtfSecond = new JTextField(5);
 	private JButton jbtStart = new JButton("START");
-	private JButton jbtPause = new JButton("PAUSE");
 	private JButton jbtStop = new JButton("STOP");
+	private JButton jbtClear = new JButton("CLEAR");
 	
 	public Push(){
 		
@@ -96,10 +112,10 @@ class Push extends JPanel{
 		p1.add(jbtStart);
 		
 		JPanel p2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		p2.add(jbtPause);
+		p2.add(jbtStop);
 		
 		JPanel p3 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		p3.add(jbtStop);
+		p3.add(jbtClear);
 		
 		setLayout(new GridLayout(1,3));
 		
@@ -107,6 +123,85 @@ class Push extends JPanel{
 		add(p2);
 		add(p3);
 		
+		jbtStart.addActionListener(new ButtonListener1());	
+		jbtStop.addActionListener(new ButtonListener2());
+		jbtClear.addActionListener(new ButtonListener3());
 	}
 	
+	 
+    private class ButtonListener1 implements ActionListener {
+		@Override /** Handle the action event */
+		public void actionPerformed(ActionEvent e) {
+			hour = Integer.valueOf(jtfHour.getText());
+			min = Integer.valueOf(jtfMinute.getText());
+			sec = Integer.valueOf(jtfSecond.getText());
+			jtfHour.setText("");
+			jtfMinute.setText("");
+			jtfSecond.setText(String.format("%.2f",));
+			eggTimer.repaint();
+		}
+	}
+	
+	private class ButtonListener2 implements ActionListener {
+		@Override /** Handle the action event */
+		public void actionPerformed(ActionEvent e) {
+			hour = Integer.valueOf(jtfHour.getText());
+			min = Integer.valueOf(jtfMinute.getText());
+			sec = Integer.valueOf(jtfSecond.getText());
+			jtfHour.setText("");
+			jtfMinute.setText("");
+			jtfSecond.setText(String.format("%.2f",getTime2()));
+			
+		
+			
+		}
+	}
+	
+	private class ButtonListener3 implements ActionListener {
+		@Override /** Handle the action event */
+		public void actionPerformed(ActionEvent e) {
+			jtfHour.setText("");
+			jtfMinute.setText("");
+			jtfSecond.setText("");
+			
+			
+			
+		}
+	}
+		
+}
+
+
+class EggTimer {
+
+    private final Timer timer = new Timer();
+    private final int minutes;
+    
+    EggTimer(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public void start() {
+        Date now = new Date();
+        timer.schedule(new RemindTask() , now, minutes * 2 * 1000);
+    }
+
+    class RemindTask extends TimerTask {
+    	int hour;
+    	int min;
+    	int sec;
+        int runtimer= hour*3600 + min*60 + sec;
+
+        public void run(){
+            if (runtimer> 0){
+                System.out.println(runtimer);
+                runtimer--;
+            } else{
+                System.out.println("--");
+                timer.cancel(); 
+            }
+        }
+    }
+   
+
 }
